d274af380b2ca3d6159655721f6688962269262e
Giguru Scheuer <giguru@uw-s.nl>
2023-07-31 17:27:18 +0200

    Add auth

---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,28 @@
+import React, {PropsWithChildren, useContext} from 'react';
+import {UserModel} from "../types/api-models";
+
+const mockUser1: UserModel = {
+    id: 'user-1',
+    name: 'Giguru Scheuer',
+};
+
+const mockUser2: UserModel = {
+    id: 'user-2',
+    name: 'Emmanuel Macron',
+};
+
+const AuthContext = React.createContext<{ user?: UserModel }>({
+    user: undefined
+});
+
+export function useAuthContext() {
+    return useContext(AuthContext);
+}
+
+export default function AuthContextProvider({ children }: PropsWithChildren<{}>) {
+    return (
+        <AuthContext.Provider value={{ user: mockUser1 }}>
+            {children}
+        </AuthContext.Provider>
+    );
+}
