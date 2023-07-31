9ca4d327536bf3f6589a25cd10daa88f672a11f1
Giguru Scheuer <giguru@uw-s.nl>
2023-07-31 21:16:45 +0200

    Remove spacing

--- src/components/wish-list/WishListMenuIndicator.tsx
+++ src/components/wish-list/WishListMenuIndicator.tsx
@@ -11,8 +11,8 @@ function WishListMenuIndicator() {
                     <div className="position-relative">
                         <Icons.FilledWishList size={30} className="text-primary" />
                         <span className="d-flex w-100 text-light justify-content-center align-items-center position-absolute z-2 start-0 top-0">
-                                            <span className="pt-1">{numberOfProducts}</span>
-                                        </span>
+                            <span className="pt-1">{numberOfProducts}</span>
+                        </span>
                     </div>
                 )
                 : <Icons.EmptyWishList size={30} /> }
