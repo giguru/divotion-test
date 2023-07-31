8c62201e0d0bd410a4c54464e72705c0bc9fc06f
Giguru Scheuer <giguru@uw-s.nl>
2023-07-31 17:26:06 +0200

    Edit icons

--- src/libs/icons.tsx
+++ src/libs/icons.tsx
@@ -1,9 +1,10 @@
-import {AiOutlineHeart, AiTwotoneHeart} from "react-icons/ai";
+import {AiFillSave, AiOutlineHeart, AiTwotoneHeart} from "react-icons/ai";
 
 // A list of icons which should be the same across the entire platform
 const Icons = {
     EmptyWishList: AiOutlineHeart,
     FilledWishList: AiTwotoneHeart,
+    Save: AiFillSave,
 }
 
 export default Icons
