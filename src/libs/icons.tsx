1e6e507047c36d7a5719a26174b8b01d125c9ffa
Giguru Scheuer <giguru@uw-s.nl>
2023-07-31 17:19:29 +0200

    Add icons

---
+++ src/libs/icons.tsx
@@ -0,0 +1,9 @@
+import {AiOutlineHeart, AiTwotoneHeart} from "react-icons/ai";
+
+// A list of icons which should be the same across the entire platform
+const Icons = {
+    EmptyWishList: AiOutlineHeart,
+    FilledWishList: AiTwotoneHeart,
+}
+
+export default Icons
